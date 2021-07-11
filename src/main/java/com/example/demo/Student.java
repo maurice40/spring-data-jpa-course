e29ed7d4ca66d66433d997e10c06630be6221ef2
maurice40 <musabirema@gmail.com>
2021-07-11 19:32:29 +0200

    Setting and updating repositories

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -23,8 +23,7 @@ public class Student {
     @Column(name = "age")
     private Integer age;
 
-    public Student(Long id, String firstName, String lastName, String email, Integer age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
